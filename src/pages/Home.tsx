630f2bf96ec59504fa34c4989ff4a5f7cd2aaf75
Troy Chaplin <troy.chaplin@gmail.com>
2026-09-06 13:27:06 -0400

    add layout and spacing classes that mirror wordpress

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,14 @@
+import { Group } from '../components/Group/Group'
+
 export function Home() {
   return (
     <>
       <h1>Troy Chaplin</h1>
       <p>Placeholder for the home page.</p>
+
+      <Group>
+        <p>Inside a group block.</p>
+      </Group>
     </>
   )
 }
